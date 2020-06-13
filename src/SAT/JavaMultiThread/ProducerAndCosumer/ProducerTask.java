a88959a674937782bb002acc0e8b5c14eb8c81e0
Mark.Wei <weixinjian2005@aliyun.com>
2020-06-13 19:28:25 +0800

    producer and consumer

---
+++ src/SAT/JavaMultiThread/ProducerAndCosumer/ProducerTask.java
@@ -0,0 +1,9 @@
+package SAT.JavaMultiThread.ProducerAndCosumer;
+
+/**
+ * @Auther: Mark Wei
+ * @Date 2020/6/13
+ * @Description: SAT.JavaMultiThread.ProducerAndCosumer
+ */
+public class ProducerTask {
+}
